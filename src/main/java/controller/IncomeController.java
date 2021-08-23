ffc9a5ac636dafaaa62e439218371f67f3a94cb1
Zlata <zlata2000@inbox.lv>
2021-08-23 21:09:39 +0300

    added expense/income option

--- src/main/java/controller/IncomeController.java
+++ src/main/java/controller/IncomeController.java
@@ -3,28 +3,51 @@ package controller;
 import Types.Category;
 import Types.InOrOut;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import model.Money;
+import model.User;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import service.MoneyService;
 import service.UserService;
 
-public class IncomeController extends ViewController{
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class IncomeController extends ViewController implements Initializable {
+@FXML
+    private ChoiceBox<String> CBCategory;
     MoneyService moneyService = new MoneyService();
 
     public TextField amountField;
-    public TextField categoryField;
-    public void  addIncome(ActionEvent actionEvent){
 
+    public void  addIncome(ActionEvent actionEvent){
+        PropertiesConfiguration p = new PropertiesConfiguration();
+        try {
+            p.load("userID.properties");
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
         try {
             Money money = new Money
                     (       InOrOut.INCOME,
-                            Category.valueOf(categoryField.getText()),
+                            Category.valueOf(CBCategory.getValue()),
                             Float.parseFloat(amountField.getText()));
 
-
             moneyService.addIncome(money);
+
+
+            int id = Integer.parseInt(p.getString("userID"));
+            User user = moneyService.getUserProfile(id);
+            user.setBudget(user.getBudget() + Float.parseFloat(amountField.getText()));
+
+            moneyService.updateBudget(user, user.getId());
+
+
 
             showAlert("Success", "Income added successfully, continue", Alert.AlertType.CONFIRMATION);
             changeScene(actionEvent, "menu");
@@ -32,5 +55,17 @@ public class IncomeController extends ViewController{
             showAlert("Adding Income Failed", e.getMessage(), Alert.AlertType.ERROR);
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        CBCategory.getItems().add(String.valueOf(Category.CAR));
+        CBCategory.getItems().add(String.valueOf(Category.EATING_OUT));
+        CBCategory.getItems().add(String.valueOf(Category.ENTERTAINMENT));
+        CBCategory.getItems().add(String.valueOf(Category.GIFTS));
+        CBCategory.getItems().add(String.valueOf(Category.GROCERIES));
+        CBCategory.getItems().add(String.valueOf(Category.TRANSPORT));
+        CBCategory.getItems().add(String.valueOf(Category.SHOPPING));
+        CBCategory.getItems().add(String.valueOf(Category.GOALS));
     }
 }
